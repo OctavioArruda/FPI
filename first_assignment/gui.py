3f203aae092ae319cb8940105d7e9ed2212a7c33
Schifer <oneiros241@gmail.com>
2017-10-01 11:47:36 -0300

    GUI working, now flips vertically and horintally the image.

--- first_assignment/gui.py
+++ first_assignment/gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFilter, ImageOps
 
 class MyFrame(Frame):
     def __init__(self):
@@ -10,13 +10,14 @@ class MyFrame(Frame):
         self.master.rowconfigure(5, weight=1)
         self.master.columnconfigure(5, weight=1)
         self.master.minsize(width = 500, height = 500)
-        #self.grid(padx=250, pady=250,sticky=W+E+N+S)
         self.pack()
 
-        self.button = Button(self, text="Open",
+        self.buttonLoad = Button(self, text="Open", # load img button
         command = self.load_img, width=10)
-        #self.button.grid(row=0, column=0,sticky=W+E+N+S)
-        self.button.pack()
+        self.buttonLoad.pack(side=LEFT)
+
+
+
 
     def load_img(self):
         imgname = askopenfilename(filetypes=(("JPG files", "*.jpg"),
@@ -29,10 +30,33 @@ class MyFrame(Frame):
                 image = Image.open(imgname)
                 tkimage = ImageTk.PhotoImage(image)
                 Label(image = tkimage).pack(side=LEFT)
+
+                self.buttonHF = Button(self,text="flipH",
+                command = lambda: self.H_flip(image),width=10)
+                self.buttonHF.pack(side=LEFT, fill=BOTH, anchor = SW) # horizontal flip button
+
+                self.buttonVF = Button(self,text="flipV",
+                command = lambda: self.V_flip(image),width=10)
+                self.buttonVF.pack(side=LEFT, fill=BOTH, anchor = SW) # vertical flip button
+
                 mainloop()
             except:
                 showerror("Open Source File", "Failed to read image\n '%s'" %imgname)
             return
+
+    def H_flip(self, image):
+        print(image.bits)
+        newimg = ImageOps.flip(image)
+        HFimg = ImageTk.PhotoImage(newimg)
+        Label(image = HFimg).pack(side=LEFT)
+        mainloop()
+
+    def V_flip(self, image):
+        print("lul")
+        newimg = ImageOps.mirror(image)
+        VFimg = ImageTk.PhotoImage(newimg)
+        Label(image = VFimg).pack(side=LEFT)
+        mainloop()
 
 if __name__=="__main__":
     MyFrame().mainloop()
