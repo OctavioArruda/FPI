b99df43505bde9fd7f13af5bb8b9b8d47e6b72db
Schifer <oneiros241@gmail.com>
2017-10-02 18:00:10 -0300

    first lab relatory. language = PTBR

--- first_assignment/gui.py
+++ first_assignment/gui.py
@@ -3,6 +3,8 @@ from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 from PIL import Image, ImageTk, ImageFilter, ImageOps
 
+image = Image.new('RGB', (800,1280), (255, 255, 255)) #global img
+
 class MyFrame(Frame):
     def __init__(self):
         Frame.__init__(self)
@@ -16,10 +18,8 @@ class MyFrame(Frame):
         command = self.load_img, width=10)
         self.buttonLoad.pack(side=LEFT)
 
-
-
-
     def load_img(self):
+        global image
         imgname = askopenfilename(filetypes=(("JPG files", "*.jpg"),
         ("PNG files", "*.png"),
         ("BMP files", "*.bmp")))
@@ -32,35 +32,43 @@ class MyFrame(Frame):
                 Label(image = tkimage).pack(side=LEFT)
 
                 self.buttonHF = Button(self,text="flipH",
-                command = lambda: self.H_flip(image),width=10)
+                command = self.H_flip,width=10)
                 self.buttonHF.pack(side=LEFT, fill=BOTH, anchor = SW) # horizontal flip button
 
                 self.buttonVF = Button(self,text="flipV",
-                command = lambda: self.V_flip(image),width=10)
+                command = self.V_flip,width=10)
                 self.buttonVF.pack(side=LEFT, fill=BOTH, anchor = SW) # vertical flip button
 
                 self.buttonGS = Button(self,text="GrayScale",
-                command = lambda: self.GSFilter(image),width=10)
+                command = self.GSFilter,width=10)
                 self.buttonGS.pack(side=LEFT, fill=BOTH, anchor = SW) # GS filter
+
+                self.buttonQT = Button(self, text="Quant",
+                command = self.QTFilter_num,width=10)
+                self.buttonQT.pack(side=LEFT, fill=BOTH, anchor = SW) # Image quantization
 
                 mainloop()
             except:
                 showerror("Open Source File", "Failed to read image\n '%s'" %imgname)
             return
 
-    def H_flip(self, image):
-        print(image.bits)
+    def H_flip(self):
+        global image
         newimg = ImageOps.flip(image)
         HFimg = ImageTk.PhotoImage(newimg)
         Label(image = HFimg).pack(side=LEFT)
+        image = newimg
         mainloop()
+        return HFimg
 
-    def V_flip(self, image):
-        print("lul")
+    def V_flip(self):
+        global image
         newimg = ImageOps.mirror(image)
         VFimg = ImageTk.PhotoImage(newimg)
         Label(image = VFimg).pack(side=LEFT)
         mainloop()
+        image = newimg
+        return VFimg
 
     # Create a new image with the given size
     def create_image(self, i, j):
@@ -78,10 +86,10 @@ class MyFrame(Frame):
         return pixel
 
     # Create a Grayscale version of the image
-    def GSFilter(self, image):
+    def GSFilter(self):
+        global image
         # Get size
         width, height = image.size
-
         # Create new Image and a Pixel Map
         new = self.create_image(width, height)
         pixels = new.load()
@@ -108,18 +116,38 @@ class MyFrame(Frame):
         GSimg = ImageTk.PhotoImage(new)
         Label(image = GSimg).pack(side=LEFT)
         mainloop()
-        # Return new image
+        image = new
+
         return new
 
-        #pix =   image.load()
-        #print (image.size)
-        #pixels = list(image.getdata()) #lista pixels
+    def process_callback(*args):
 
-        #print("GrayScale Filter")
-        #newimg = ImageOps.grayscale(image)
-        #GSimg = ImageTk.PhotoImage(newimg)
-        #Label(image = GSimg).pack(side=LEFT)
-        mainloop()
+    # figure out what the response to the input should be
+        response = process_command(entry.get())
+
+    def QTFilter_num(self):
+        # quantization number input
+        numQuant = Tk()
+        numQuant.title("Quantization")
+
+        LblQuant = Label(numQuant, text = "Quantization number:")
+        LblQuant.pack(side=LEFT)
+
+        numQT = IntVar()
+        EQuant = Entry(numQuant , bd=5, textvariable = numQT)
+        EQuant.focus()
+        EQuant.bind('<Return>', self.process_callback)
+        EQuant.pack(side=LEFT)
+        print(numQT.get())
+        print(numQT)
+        BTQT = Button(numQuant, text="Ok", command = lambda: self.QTFilter(num))
+        BTQT.pack(side=RIGHT)
+
+
+    def QTFilter(self, num):
+        print(num)
+        global image
+
 
 if __name__=="__main__":
     MyFrame().mainloop()
