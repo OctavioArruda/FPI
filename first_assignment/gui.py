2ab7597870a31c84c5c52053f6fbd11a0485bdcf
Schifer <oneiros241@gmail.com>
2017-10-04 18:08:55 -0300

    Interface looks better

--- first_assignment/gui.py
+++ first_assignment/gui.py
@@ -8,15 +8,15 @@ image = Image.new('RGB', (800,1280), (255, 255, 255)) #global image
 class MyFrame(Frame):
     def __init__(self):
         Frame.__init__(self)
-        self.master.title("Image editor")
+        self.master.title("ImageShop")
         self.master.rowconfigure(5, weight=1)
         self.master.columnconfigure(5, weight=1)
         self.master.minsize(width = 600, height = 500)
-        self.pack()
+        self.grid()
 
         self.buttonLoad = Button(self, text="Open", # load img button
         command = self.load_img, width=10)
-        self.buttonLoad.pack(side=LEFT)
+        self.buttonLoad.grid()
 
     def load_img(self):
         global image
@@ -29,42 +29,42 @@ class MyFrame(Frame):
                 print(imgname)
                 image = Image.open(imgname)
                 tkimage = ImageTk.PhotoImage(image)
-                Label(image = tkimage).pack(side=LEFT)
+                Label(image = tkimage).grid(row = 0, column = 8,padx = 10, pady= 10)
 
                 self.buttonHF = Button(self,text="flipH",
                 command = self.H_flip,width=10)
-                self.buttonHF.pack(side=LEFT, fill=BOTH, anchor = SW) # horizontal flip button
+                self.buttonHF.grid(row = 1, column = 0, padx = 10, pady = 1) # horizontal flip button
 
                 self.buttonVF = Button(self,text="flipV",
                 command = self.V_flip,width=10)
-                self.buttonVF.pack(side=LEFT, fill=BOTH, anchor = SW) # vertical flip button
+                self.buttonVF.grid(row = 2, column = 0, padx = 10, pady = 1) # vertical flip button
 
                 self.buttonGS = Button(self,text="GrayScale",
                 command = self.GSFilter,width=10)
-                self.buttonGS.pack(side=LEFT, fill=BOTH, anchor = SW) # GS filter
+                self.buttonGS.grid(row = 3, column = 0, padx = 10, pady = 1) # GS filter
 
                 self.LblQT = Label(self, text = "QT number:") # label qt
-                self.LblQT.pack(side=LEFT)
+                self.LblQT.grid(row = 4, column = 0, padx = 1, pady =1)
 
-                self.EQT = Entry(self, bd =3, width = 3) # Image quantization
-                self.EQT.bind("<Return>",lambda num: self.QTFilter(num = self.EQT.get()))
-                self.EQT.pack(side=LEFT)
+                self.EQT = Entry(self, bd =3, width = 10) # Image quantization
+                self.EQT.bind("<Return>",lambda numqt: self.QTFilter(numqt = self.EQT.get()))
+                self.EQT.grid(row = 4, column = 1)
 
                 self.buttonQT = Button(self, text="Ok",
-                command = lambda: self.QTFilter(num = self.EQT.get()), width=5)
-                self.buttonQT.pack(side=LEFT, fill=BOTH, anchor = SW) # Image qt button
+                command = lambda: self.QTFilter(numqt = self.EQT.get()), width=5)
+                self.buttonQT.grid(row = 4, column = 2, ) # Image qt button
 
 
                 self.LblSV = Label(self, text = "Save as:") # label qt
-                self.LblSV.pack(side=LEFT)
+                self.LblSV.grid(row = 5, column = 0)
 
                 self.ESV = Entry(self, bd =3, width = 10) # Image quantization
                 self.ESV.bind("<Return>",lambda name: self.save_img(name = self.ESV.get()))
-                self.ESV.pack(side=LEFT)
+                self.ESV.grid(row = 5, column = 1)
 
                 self.buttonSV = Button(self, text="Save",
                 command = lambda: self.save_img(name = self.ESV.get()),width=5)
-                self.buttonSV.pack(side=LEFT, fill=BOTH, anchor = SW)
+                self.buttonSV.grid(row = 5, column = 2)
 
                 mainloop()
             except:
@@ -83,7 +83,7 @@ class MyFrame(Frame):
                 image.putpixel((x, y), right)
 
         HFimg = ImageTk.PhotoImage(image)
-        Label(image = HFimg).pack(side=LEFT)
+        Label(image = HFimg).grid(row = 0, column = 20)
         mainloop()
         return HFimg
 
@@ -100,7 +100,7 @@ class MyFrame(Frame):
                 image.putpixel((x, y), right)
 
         VFimg = ImageTk.PhotoImage(image)
-        Label(image = VFimg).pack(side=LEFT)
+        Label(image = VFimg).grid(row = 0, column = 20)
         mainloop()
         return VFimg
 
@@ -161,7 +161,7 @@ class MyFrame(Frame):
 
         # Show image
         GSimg = ImageTk.PhotoImage(new)
-        Label(image = GSimg).pack(side=LEFT)
+        Label(image = GSimg).grid(row = 0, column = 20)
 
         image = new
 
@@ -169,17 +169,55 @@ class MyFrame(Frame):
 
         return new
 
-    def QTFilter(self, num):
+    def QTFilter(self, numqt):
         global image
-        new = image.quantize(int(num), None, 0, None)
-        QTimg = ImageTk.PhotoImage(new)
-        Label(image = QTimg).pack(side=LEFT)
+        width, height = image.size
+        newimg = self.create_image(width, height)
+        newpixels = newimg.load()
 
-        image = new
+        num = int(numqt)
+
+        if num > 256:
+            num = 256;
+        elif num <= 0:
+            num = 1;
+
+        prop = 256 / num
+
+        for i in range(width):
+            for j in range(height):
+                pixel = self.get_pixel(image, i, j)
+
+                Red = pixel[0]
+                Green = pixel[1]
+                Blue = pixel[2]
+
+                Rchan = Red / prop
+                Gchan = Green / prop
+                Bchan = Blue / prop
+
+                newR = Rchan * prop + (prop / 2)
+                newG = Gchan * prop + (prop / 2)
+                newB = Bchan * prop + (prop / 2)
+
+                if newR > 255:
+                    newR = 255;
+                if newG > 255:
+                    newG = 255;
+                if newB > 255:
+                    newB = 255;
+
+                newpixels[i, j] = (int(newR), int(newG), int(newB))
+
+        #new = image.quantize(int(num), None, 0, None)
+        QTimg = ImageTk.PhotoImage(newimg)
+        Label(image = QTimg).grid(row = 0, column = 20)
+
+        image = newimg
 
         mainloop()
 
-        return new
+        return newimg
 
 if __name__=="__main__":
     MyFrame().mainloop()
