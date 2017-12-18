44e19a388313522a6872cb83aeec2d8b6248012a
Schifer <oneiros241@gmail.com>
2017-12-18 17:14:35 -0200

    nearest to the end , some error on D calc

--- FPIFINAL/down.m
+++ FPIFINAL/down.m
@@ -1,8 +1,12 @@
+
 function down(image, dsfactor) % patchsize constant = 4
 
     H = imread(image); 
     %H = rgb2gray(H);
     [Rows, Cols, Channels] = size(H);
+    
+    
+    
     G = [0.0625 0.125 0.0625; 0.125 0.25 0.125; 0.0625 0.125 0.0625] ;
     np = [0.25 0.25; 0.25 0.25]; % patchsize 2x2 always
     
@@ -74,14 +78,11 @@ function down(image, dsfactor) % patchsize constant = 4
     end
     output = imresize(exites,[Rows/2 Cols/2]);
     L2 = output;
-
+    
     % 4. now a convolution with the patchsize
     
     M = conv2(L, np, 'valid');
     
-    subplot(3,3,3);
-    imshow(uint8(M));
-    title('M')
     % 5.
     Sl = imresize(L, 2);
 
@@ -107,79 +108,114 @@ function down(image, dsfactor) % patchsize constant = 4
     title('Sh-Ms')
     
     % 7.
-    X = sqrt(Sh ./ Sl);
-    R = imresize(X, 0.5);
+    X = Sh ./ Sl;
+    
+    subplot(3,3,1);
+    imshow(uint8(X));
+    title('X = Sh./Sl');
+    
     
     % 8. 
-    [XRols,XCols] = size(R);
+    [XRols,XCols] = size(X);
     for j = 1:XRols
       for i = 1:XCols
-        if X(j,i) <  0.000001
+        k = X(j,i) - Sl(j,i);
+        if k <  0.000001
           X(j,i) = 0;
         end
       end
     end
-      
+     R = imresize(X, 0.5); 
+     
     % 9.
     
     Im = uint8(ones(size(M)));
     N = conv2(Im, np, 'full');
-    
-    subplot(3,3,6)
-    imshow(uint8(X));
-    title('X');
+    subplot(3,3,2);
+    imshow(uint8(N));
+    title('N(conv2(Im, np))')
+   
     
     % 10.
     [RowsR,ColsR] = size(R);
     R = imresize(R,[RowsR-1 ColsR-1]);
     
-    T = conv2(R .* M, np, 'full');
-
+    T = conv2(double(abs(R)) .* double(abs(M)), np, 'full');
+    subplot(3,3,3);
+    imshow(uint8(T));
+    title('T= conv2(R.*M,np)');
     
     % 11.
     
     M  = conv2(M, np, 'full');
-    subplot(3,3,1);
+    
+    subplot(3,3,6);
     imshow(uint8(M));
-    title('M');
+    title('M=conv(M,np))');
     
     % 12.
     
     R = conv2(R, np, 'full');
     
-
+    subplot(3,3,8)
+    imshow(uint8(R));
+    title('R');
     % 13. end
     
     [mr, mc] = size(M);
-
+    disp(size(M));
+    disp(size(R));
+    disp(size(L));
+    disp(size(T));
+    disp(size(N));
     %R = reshape(M, mr, mc);
     %L = reshape(M, mr, mc);
     %T = reshape(M, mr, mc);
     %N = reshape(M, mr, mc);
+    subplot(3,3,7);
+    imshow(uint8(L));
+    title('L used in D');
     
-    D = uint8(abs(R)) .* uint8(abs(L));
-    subplot(3,3,7)
+    subplot(3,3,7);
+    imshow(uint8(R));
+    title('R before R .* L');
+    
+    subplot(3,3,6);
+    imshow(uint8(L));
+    title('L before R.* L');
+    D = double(abs(R)) .* double(abs(L));
+    figure
+    subplot(3,3,1);
     imshow(uint8(D));
-    title('D .* L');
-    
-    D = uint8(M) + uint8(D);
-    subplot(3,3,8);
-    imshow(uint8(D));
-    title(' M + D( R.* L)');
-    
-    D = uint8(abs(D)) - uint8(abs(T));
-    subplot(3,3,9);
-    imshow(uint8(D));
-    title(' M+D(R .* L) - T');
-    
-    D = uint8(D) ./ uint8(N);
-    
-        
-    subplot(3,3,2);
-    imshow(uint8(D));
-    title('(M+D+R*L-T)/N');
+    title('D=R.*L');
     
 
+    D = double(M) + double(D);
+    subplot(3,3,2);
+    imshow(uint8(D));
+    title('D=M+(R.*L)');
+    
+    D = double(D) - double(T);
+    subplot(3,3,3);
+    imshow(uint8(D));
+    title('D=M+(R.*L)-T');
+    
+    subplot(3,3,5);
+    imshow(uint8(N));
+    title('N');
+    
+    D = double(D) ./ double(N);
+    subplot(3,3,4);
+    imshow(uint8(D));
+    title('D=M+(R.*L)-T./N');
+   
+    
+    
+    %D = (uint8(M) + (uint8(abs(R)) .* uint8(abs(L))) - uint8(T)) ./ uint8(N); 
+    
+    subplot(3,3,9);
+    imshow(uint8(D));
+    title('downscalled');
     % show me truth with mangekÿo sharingan
     %figure
     %subplot(1,2,1);
@@ -188,7 +224,11 @@ function down(image, dsfactor) % patchsize constant = 4
     %subplot(1,2,2);
     %title('downscalledboi');
     %imshow(uint8(D));
+    
+    subplot(3,3,8);
+    imshow(uint8(H));
+    title('original');
+    
+    
+  
 end
-  
-  
-  
