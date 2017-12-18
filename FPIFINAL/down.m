75bf86435b368446634ed20ce0ca6947f2a278c1
Schifer <oneiros241@gmail.com>
2017-12-18 18:24:03 -0200

    working shit percetually downscalle of images

--- FPIFINAL/down.m
+++ FPIFINAL/down.m
@@ -1,13 +1,14 @@
 
-function down(image, dsfactor) % patchsize constant = 4
+function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
 
     H = imread(image); 
-    %H = rgb2gray(H);
+    %H = rgb2gray(H); 
     [Rows, Cols, Channels] = size(H);
     
     
     
     G = [0.0625 0.125 0.0625; 0.125 0.25 0.125; 0.0625 0.125 0.0625] ;
+    
     np = [0.25 0.25; 0.25 0.25]; % patchsize 2x2 always
     
     % testing the downscalling factor, so we can get a kernel for median
@@ -25,22 +26,23 @@ function down(image, dsfactor) % patchsize constant = 4
             0.0625 0.0625 0.0625 0.0625; 0.0625 0.0625 0.0625 0.0625];
     end
     
-    % 1.first we do a convolution with a low pass kernel and the input_img
-    L = conv2(H, kernel, 'valid');
+    % 2 convolution H, P(s)
+    Temp = conv2(H, kernel, 'valid');
     
-    % 2.then we subsample this convolution result with factor pixels
-
+    % 2.2 SubSample(ConvValid(H, P(s)), s)
+    % SubSample(Temp, P(s)
     
-    [Rows, Cols, Channels] = size(L);
-    exites = uint8(zeros(size(L)));
+    % SubSample procedure
+    [Rows, Cols, Channels] = size(Temp);
+    exites = uint8(zeros(size(Temp)));
     for x = 1:2:Rows
         for y = 1:2:Cols
           if x >= Rows 
-            k =  L(x,y);
+            k =  Temp(x,y);
           elseif y >= Cols 
-            k =  L(x,y);
+            k =  Temp(x,y);
           else
-            k =  (L(x,y) + L(x+1,y) + L(x,y+1) + L(x+1,y+1))/4;
+            k =  (Temp(x,y) + Temp(x+1,y) + Temp(x,y+1) + Temp(x+1,y+1))/4;
           end
           exites(x,y) =  k;
           exites(x+1,y) =  k;
@@ -49,26 +51,35 @@ function down(image, dsfactor) % patchsize constant = 4
         end       
     end
     output = imresize(exites,[Rows/2 Cols/2]);
-    L = output;
     
+    L = output; % L = SubSample(ConvValid(H, P(s)), s);
+    
+    % start the prints:
     figure
+    subplot(4,4,1);
+    imshow(uint8(L));
+    title('uint8(L);');
+   
+    % 3. Resize H for H²
 
+    H2 = imresize(H, 2); 
     
-    % 3.another convolution now with the input image scalled by a *2
+    % 3.1 Temp2 = ConvValid(H2, P(s));
     
-    L2 = imresize(H, 2);
-    L2 = conv2(L2, kernel, 'valid');
+    Temp2 = conv2(H2, kernel, 'valid');
     
-    [Rows, Cols, Channels] = size(L2);
-    exites = uint8(zeros(size(L2)));
+    % 3.2 L2 = SubSample(Temp2, s);
+    
+    [Rows, Cols, Channels] = size(Temp2);
+    exites = uint8(zeros(size(Temp2)));
     for x = 1:2:Rows
         for y = 1:2:Cols
           if x >= Rows 
-            k =  L2(x,y);
+            k =  Temp2(x,y);
           elseif y >= Cols 
-            k =  L2(x,y);
+            k =  Temp2(x,y);
           else
-            k =  (L2(x,y) + L2(x+1,y) + L2(x,y+1) + L2(x+1,y+1))/4;
+            k =  (Temp2(x,y) + Temp2(x+1,y) + Temp2(x,y+1) + Temp2(x+1,y+1))/4;
           end
           exites(x,y) =  k;
           exites(x+1,y) =  k;
@@ -77,158 +88,166 @@ function down(image, dsfactor) % patchsize constant = 4
         end       
     end
     output = imresize(exites,[Rows/2 Cols/2]);
+    
     L2 = output;
     
-    % 4. now a convolution with the patchsize
+    % L2 print
+    
+    subplot(4,4,2);
+    imshow(uint8(L2));
+    title('uint8(L2);');
+    
+    % 4. M = ConvValid(L , np );
     
     M = conv2(L, np, 'valid');
     
-    % 5.
-    Sl = imresize(L, 2);
-
-    Sl = conv2(Sl, np, 'valid');
+    % 5. Resize L for L²
     
-    Ms = imresize(M, 2);
-    [RowsMs,ColsMs] = size(Ms);
-    Ms = imresize(Ms,[RowsMs+1 ColsMs+1]);
-    Sl = Sl - Ms;
-    subplot(3,3,4)
+    Lsquare = imresize(L, 2);
+    
+    % 5.1 Sl = ConvValid(Lsquare, np);
+    
+    Sl = conv2(Lsquare, np, 'valid');
+    
+    subplot(4,4,3);
     imshow(uint8(Sl));
-    title('Sl - Ms');
-
+    title('uint8(Sl)');
     
+    % 5.2 resize M for M²
+    
+    M2 = imresize(M, 2);
+    
+    % 5.3 Sl = ConvValid(Lsquare,np) - M2;
+    
+     [TempR,TempC] = size(Sl);
+     Sl = imresize(Sl,[TempR-1 TempC-1]);
    
-    % 6.
+    Sl = Sl - M2;
+    
+    subplot(4,4,4);
+    imshow(uint8(Sl));
+    title('uint8(Sl - M2)');
+    
+    % 6. Sh = ConvValid(L2, np);
     
     Sh = conv2(L2, np, 'valid');
-    Sh = Sh - Ms;
     
-    subplot(3,3,5)
+    subplot(4,4,5);
     imshow(uint8(Sh));
-    title('Sh-Ms')
+    title('uint8(Sh)');
     
-    % 7.
-    X = Sh ./ Sl;
+    % 6.1 Sh = ConvValid(L2, np) - M2;
     
-    subplot(3,3,1);
-    imshow(uint8(X));
-    title('X = Sh./Sl');
+    [TempR,TempC] = size(Sh);
+    Sh = imresize(Sh,[TempR-1 TempC-1]);
     
+    Sh = Sh - M2;
     
-    % 8. 
-    [XRols,XCols] = size(X);
+    subplot(4,4,6);
+    imshow(uint8(Sh));
+    title('uint8(Sh - M2)');
+    
+    % 7. Rtemp = Sh ./ Sl
+    
+    Rtemp = Sh ./ Sl;
+    
+    subplot(4,4,7);
+    imshow(uint8(Rtemp));
+    title('uint8(Rtemp = Sh ./ Sl)');
+    
+    % 7.1 sqrt(Rtemp);
+    
+    Rtemp = sqrt(Rtemp);
+    
+    subplot(4,4,8);
+    imshow(uint8(Rtemp));
+    title('uint8(Rtemp = sqrt(Rtemp))');
+    
+    % 8. R(Sl < e) <- 0
+    
+    [XRols,XCols] = size(Rtemp);
     for j = 1:XRols
       for i = 1:XCols
-        k = X(j,i) - Sl(j,i);
+        k = Rtemp(j,i) - Sl(j,i);
         if k <  0.000001
-          X(j,i) = 0;
+          Rtemp(j,i) = 0;
         end
       end
     end
-     R = imresize(X, 0.5); 
+    R = imresize(Rtemp, 0.5); 
+    
+    subplot(4,4,9);
+    imshow(uint8(R));
+    title('uint8(R)');
+    
+    % 9. N = convFull(Im, np)
+    
+     Im = uint8(ones(size(M)));
      
-    % 9.
+     N = conv2(Im, np, 'full');
+     
+     subplot(4,4,10);
+     imshow(uint8(N));
+     title('uint8(N)');
     
-    Im = uint8(ones(size(M)));
-    N = conv2(Im, np, 'full');
-    subplot(3,3,2);
-    imshow(uint8(N));
-    title('N(conv2(Im, np))')
-   
+    % 10. RXM = R .* M.
+    RXM = R .* M;
     
-    % 10.
-    [RowsR,ColsR] = size(R);
-    R = imresize(R,[RowsR-1 ColsR-1]);
+    subplot(4,4,11);
+    imshow(uint8(RXM));
+    title('uint8(RXM)');
     
-    T = conv2(double(abs(R)) .* double(abs(M)), np, 'full');
-    subplot(3,3,3);
+    % 10.1 T = convFull(RXM, np);
+    
+    T = conv2(RXM, np, 'full');
+    
+    subplot(4,4,12);
     imshow(uint8(T));
-    title('T= conv2(R.*M,np)');
+    title('uint8(T)');
     
-    % 11.
+    % 11. M = convFull(M, np)
+             
+    M = conv2(M, np,'full');
     
-    M  = conv2(M, np, 'full');
-    
-    subplot(3,3,6);
+    subplot(4,4,13);
     imshow(uint8(M));
-    title('M=conv(M,np))');
+    title('uint8(M) conv2(M, np)');
     
-    % 12.
+    % 12. R = convFull(Rtemp, np);
     
     R = conv2(R, np, 'full');
     
-    subplot(3,3,8)
+    subplot(4,4,14);
     imshow(uint8(R));
-    title('R');
-    % 13. end
+    title('uint8(R) conv2(Rtemp, np)');
     
-    [mr, mc] = size(M);
-    disp(size(M));
-    disp(size(R));
-    disp(size(L));
-    disp(size(T));
-    disp(size(N));
-    %R = reshape(M, mr, mc);
-    %L = reshape(M, mr, mc);
-    %T = reshape(M, mr, mc);
-    %N = reshape(M, mr, mc);
-    subplot(3,3,7);
-    imshow(uint8(L));
-    title('L used in D');
+    % 13.1 RL = R .* L;
     
-    subplot(3,3,7);
-    imshow(uint8(R));
-    title('R before R .* L');
+    RL = double(R) .* double(L);
     
-    subplot(3,3,6);
-    imshow(uint8(L));
-    title('L before R.* L');
-    D = double(abs(R)) .* double(abs(L));
-    figure
-    subplot(3,3,1);
+    subplot(4,4,15);
+    imshow(uint8(RL));
+    title('uint8(RL) R .* L');
+    
+    % 13.2 MRL = M + RL;
+    
+    MRL = M + RL;
+    
+    % 13.3 MRLT = MRL - T;
+    
+    MRLT = MRL - T;
+    
+    % 13.4 MRLTN = MRLT ./ N;
+    
+    MRLTN = MRLT ./ N;
+    
+    D = MRLTN;
+    
+    subplot(4,4,16);
     imshow(uint8(D));
-    title('D=R.*L');
+    title('uint8(D) downscalled)');
     
-
-    D = double(M) + double(D);
-    subplot(3,3,2);
-    imshow(uint8(D));
-    title('D=M+(R.*L)');
+    %disp(size(D)); % downscalled img 128x128 vs 256x256 original image!
+    %disp(size(H));
     
-    D = double(D) - double(T);
-    subplot(3,3,3);
-    imshow(uint8(D));
-    title('D=M+(R.*L)-T');
-    
-    subplot(3,3,5);
-    imshow(uint8(N));
-    title('N');
-    
-    D = double(D) ./ double(N);
-    subplot(3,3,4);
-    imshow(uint8(D));
-    title('D=M+(R.*L)-T./N');
-   
-    
-    
-    %D = (uint8(M) + (uint8(abs(R)) .* uint8(abs(L))) - uint8(T)) ./ uint8(N); 
-    
-    subplot(3,3,9);
-    imshow(uint8(D));
-    title('downscalled');
-    % show me truth with mangekÿo sharingan
-    %figure
-    %subplot(1,2,1);
-    %title('original');
-    %imshow(H);
-    %subplot(1,2,2);
-    %title('downscalledboi');
-    %imshow(uint8(D));
-    
-    subplot(3,3,8);
-    imshow(uint8(H));
-    title('original');
-    
-    
-  
 end
