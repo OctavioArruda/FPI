ea602e9d1a653bb8a933aa391da17616fdaad37e
Schifer <oneiros241@gmail.com>
2017-12-19 13:45:03 -0200

    final version perceptually based downscalling

--- FPIFINAL/down.m
+++ FPIFINAL/down.m
@@ -15,6 +15,7 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     % filter which is a low pass filter. if the factor is not int, he
     % should be truncated. Kernels with 1 valeu means no downscalling at
     % all. Kernels above 5 should make image so small that we can even see.
+    
     if round(dsfactor) == 2
         kernel = [ 0.25 0.25 ; 0.25 0.25 ]; end
     
@@ -55,10 +56,10 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     L = output; % L = SubSample(ConvValid(H, P(s)), s);
     
     % start the prints:
-    figure
-    subplot(4,4,1);
-    imshow(uint8(L));
-    title('uint8(L);');
+    %figure
+    %subplot(4,4,1);
+    %imshow(uint8(L));
+    %title('uint8(L);');
    
     % 3. Resize H for H²
 
@@ -93,9 +94,9 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     
     % L2 print
     
-    subplot(4,4,2);
-    imshow(uint8(L2));
-    title('uint8(L2);');
+    %subplot(4,4,2);
+    %imshow(uint8(L2));
+    %title('uint8(L2);');
     
     % 4. M = ConvValid(L , np );
     
@@ -109,9 +110,9 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     
     Sl = conv2(Lsquare, np, 'valid');
     
-    subplot(4,4,3);
-    imshow(uint8(Sl));
-    title('uint8(Sl)');
+    %subplot(4,4,3);
+    %imshow(uint8(Sl));
+    %title('uint8(Sl)');
     
     % 5.2 resize M for M²
     
@@ -124,17 +125,17 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
    
     Sl = Sl - M2;
     
-    subplot(4,4,4);
-    imshow(uint8(Sl));
-    title('uint8(Sl - M2)');
+    %subplot(4,4,4);
+    %imshow(uint8(Sl));
+    %title('uint8(Sl - M2)');
     
     % 6. Sh = ConvValid(L2, np);
     
     Sh = conv2(L2, np, 'valid');
     
-    subplot(4,4,5);
-    imshow(uint8(Sh));
-    title('uint8(Sh)');
+    %subplot(4,4,5);
+    %imshow(uint8(Sh));
+    %title('uint8(Sh)');
     
     % 6.1 Sh = ConvValid(L2, np) - M2;
     
@@ -143,25 +144,25 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     
     Sh = Sh - M2;
     
-    subplot(4,4,6);
-    imshow(uint8(Sh));
-    title('uint8(Sh - M2)');
+    %subplot(4,4,6);
+    %imshow(uint8(Sh));
+    %title('uint8(Sh - M2)');
     
     % 7. Rtemp = Sh ./ Sl
     
     Rtemp = Sh ./ Sl;
     
-    subplot(4,4,7);
-    imshow(uint8(Rtemp));
-    title('uint8(Rtemp = Sh ./ Sl)');
+    %subplot(4,4,7);
+    %imshow(uint8(Rtemp));
+    %title('uint8(Rtemp = Sh ./ Sl)');
     
     % 7.1 sqrt(Rtemp);
     
     Rtemp = sqrt(Rtemp);
     
-    subplot(4,4,8);
-    imshow(uint8(Rtemp));
-    title('uint8(Rtemp = sqrt(Rtemp))');
+    %subplot(4,4,8);
+    %imshow(uint8(Rtemp));
+    %title('uint8(Rtemp = sqrt(Rtemp))');
     
     % 8. R(Sl < e) <- 0
     
@@ -176,58 +177,58 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     end
     R = imresize(Rtemp, 0.5); 
     
-    subplot(4,4,9);
-    imshow(uint8(R));
-    title('uint8(R)');
+    %subplot(4,4,9);
+    %imshow(uint8(R));
+    %title('uint8(R)');
     
     % 9. N = convFull(Im, np)
     
-     Im = uint8(ones(size(M)));
+    Im = uint8(ones(size(M)));
      
-     N = conv2(Im, np, 'full');
+    N = conv2(Im, np, 'full');
      
-     subplot(4,4,10);
-     imshow(uint8(N));
-     title('uint8(N)');
+    %subplot(4,4,10);
+    %imshow(uint8(N));
+    %title('uint8(N)');
     
     % 10. RXM = R .* M.
     RXM = R .* M;
     
-    subplot(4,4,11);
-    imshow(uint8(RXM));
-    title('uint8(RXM)');
+    %subplot(4,4,11);
+    %imshow(uint8(RXM));
+    %title('uint8(RXM)');
     
     % 10.1 T = convFull(RXM, np);
     
     T = conv2(RXM, np, 'full');
     
-    subplot(4,4,12);
-    imshow(uint8(T));
-    title('uint8(T)');
+    %subplot(4,4,12);
+    %imshow(uint8(T));
+    %title('uint8(T)');
     
     % 11. M = convFull(M, np)
              
     M = conv2(M, np,'full');
     
-    subplot(4,4,13);
-    imshow(uint8(M));
-    title('uint8(M) conv2(M, np)');
+    %subplot(4,4,13);
+    %imshow(uint8(M));
+    %title('uint8(M) conv2(M, np)');
     
     % 12. R = convFull(Rtemp, np);
     
     R = conv2(R, np, 'full');
     
-    subplot(4,4,14);
-    imshow(uint8(R));
-    title('uint8(R) conv2(Rtemp, np)');
+    %subplot(4,4,14);
+    %imshow(uint8(R));
+    %title('uint8(R) conv2(Rtemp, np)');
     
     % 13.1 RL = R .* L;
     
     RL = double(R) .* double(L);
     
-    subplot(4,4,15);
-    imshow(uint8(RL));
-    title('uint8(RL) R .* L');
+    %subplot(4,4,15);
+    %imshow(uint8(RL));
+    %title('uint8(RL) R .* L');
     
     % 13.2 MRL = M + RL;
     
@@ -243,11 +244,25 @@ function down(image, dsfactor) % patchsize constant = 4 > 2x2 kernel
     
     D = MRLTN;
     
-    subplot(4,4,16);
-    imshow(uint8(D));
-    title('uint8(D) downscalled)');
+    %subplot(4,4,16);
+    %imshow(uint8(D));
+    %title('uint8(D) downscalled)');
     
     %disp(size(D)); % downscalled img 128x128 vs 256x256 original image!
     %disp(size(H));
+    
+    figure
+    subplot(1,2,1);
+    imshow(uint8(H));
+    axis on
+    title('Input Image');
+    
+    subplot(1,2,2);
+    imshow(uint8(abs(D)))
+    axis on
+    title('DownScalled Image');
+        
+
+    
     
 end
